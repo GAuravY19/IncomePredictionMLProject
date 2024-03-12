8a6f4a43f114e81b2f013643de337772e7670c87
gaurav <gauravy1905@gmail.com>
2024-03-12 11:44:30 +0530

    Completed the data cleaning process

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import numpy as np
 from src.utils.movedata import MoveData
 from src.utils.savedata import SavingData
 from src.components.dataPreparation import DataPrepare
+from src.components.dataclean import CleaningData
 from src.exceptions import CustomException
 from src.logger import logging
 
@@ -25,5 +26,18 @@ df = prepare.readingData(path=PATH)
 
 FILE_LOCATION = "D:/IncomeML Model/data/processed"
 SavingData(df, location=FILE_LOCATION, file_name='data.csv')
+
+
+clean = CleaningData(df)
+
+df1 = clean.Step_01_workclass(df)
+df2 = clean.Step_02_education(df1)
+df3 = clean.step_03_marital_status(df2)
+df4 = clean.step_04_occupation(df3)
+df5 = clean.step_05(df4)
+df6 = clean.step_06(df5)
+
+SavingData(df6, location=FILE_LOCATION, file_name="clean_data.csv")
+
 
 print("main file completed working.")
